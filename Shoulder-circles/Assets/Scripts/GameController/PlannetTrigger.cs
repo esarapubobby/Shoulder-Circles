627d04a2264010becc7319794573e90f2271d05f
BadriRajuPericharla <bhagavanpericharla@gmail.com>
2026-04-04 16:10:23 +0530

    Modified Textures

--- Shoulder-circles/Assets/Scripts/GameController/PlannetTrigger.cs
+++ Shoulder-circles/Assets/Scripts/GameController/PlannetTrigger.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlannetTrigger : MonoBehaviour
 {
+    public float rotationspeed=0.05f;
+    void Update()
+    {
+        transform.Rotate(0,rotationspeed,0);
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "SpaceCraft")
